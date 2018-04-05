4f173834b7df925a3f6c0cbd7e55b32e1065f8ba
lisa d oatway <lisaoatway@hotmail.com>
2018-04-05 19:09:32 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function myFunction1() {
+    alert(document.getElementsByTagName("p").length);
+}
+
+function myFunction2() {
+   var x= document.getElementsById("inner");
+   alert(x.length)
+}
+
+function myFunction() {
+   var y= document.getElementsById("end");
+    alert(y.length)
+}
