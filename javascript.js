2bd5ab706ad9e1984903d7906f43e683a0e4b0c3
lisa d oatway <lisaoatway@hotmail.com>
2018-04-05 20:28:42 -0400

    fixed bugs

--- javascript.js
+++ javascript.js
@@ -3,11 +3,13 @@ function myFunction1() {
 }
 
 function myFunction2() {
-   var x= document.getElementsById("inner");
-   alert(x.length)
+   var x= document.getElementById("inner");
+    var y = x.getElementsByTagName("p").length;
+   alert("there are "+ y+ " inner elements");
 }
 
-function myFunction() {
-   var y= document.getElementsById("end");
-    alert(y.length)
+function myFunction3() {
+   var z= document.getElementById("end");
+    var w = z.getElementsByTagName("p").length;
+    alert("there are "+ w+ " end elements")
 }
